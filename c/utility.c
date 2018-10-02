c1f246031b9f67fbce7362a7a0646b4110203542
Jia Guo <jia.guo@wmich.edu>
2018-10-02 00:31:52 -0400

    moved frequently and commonly used functions to utility.c and created utility.h

---
+++ c/utility.c
@@ -0,0 +1,31 @@
+#include "utility.h"
+
+// randomly fill the arr with unique numbers within the range of [1, size]
+void init_arr(int * arr, int size) {
+    srand(time(NULL));
+    int i;
+    int j;
+    // lib is initialized with values from 1 to size in order
+    int *lib = (int*)malloc(size * sizeof(int));
+    for (i = 0; i < size; i++) {
+        lib[i] = i + 1;
+    }
+    // find unused values using randomly generated index
+    for (i = 0; i < size; i++) {
+        do {
+            j = rand() % size;
+        } while (lib[j] == 0);
+        arr[i] = lib[j];
+        // set used number to 0
+        lib[j] = 0;
+    }
+    free(lib);
+}
+
+void print_arr(int * arr, int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
---
+++ c/utility.h
@@ -0,0 +1,14 @@
+#ifndef _UTILITY_H
+#define _UTILITY_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <limits.h>
+
+/* Function prototypes */
+void init_arr(int * arr, int size);
+void print_arr(int * arr, int size);
+
+#endif
